3d38ec5db072a9a8cb663a49afe2f9d9d54b2350
kdlibiran <kglibiran@up.edu.ph>
2024-11-09 21:22:45 +0800

    test

--- function3/function/app.py
+++ function3/function/app.py
@@ -1,10 +1,40 @@
 import json
 
 def lambda_handler(event, context):
-    print(event)
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello from function3",
-        }),
-    }
+    # Get the HTTP method from the event
+    http_method = event.get('httpMethod')
+    
+    if http_method == 'GET':
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Hello from function3 - GET request"
+            })
+        }
+    
+    elif http_method == 'POST':
+        # Get the request body
+        try:
+            body = json.loads(event.get('body', '{}'))
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                    "message": "Hello from function3 - POST request",
+                    "received_data": body
+                })
+            }
+        except json.JSONDecodeError:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({
+                    "message": "Invalid JSON in request body"
+                })
+            }
+    
+    else:
+        return {
+            "statusCode": 405,
+            "body": json.dumps({
+                "message": "Method not allowed"
+            })
+        }
