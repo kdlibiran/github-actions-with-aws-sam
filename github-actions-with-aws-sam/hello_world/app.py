09b740c84f744e0818526cb5d2fcc42c925f5223
kdlibiran <kglibiran@up.edu.ph>
2024-11-09 16:47:29 +0800

    Change hashing

--- github-actions-with-aws-sam/hello_world/app.py
+++ github-actions-with-aws-sam/hello_world/app.py
@@ -1,13 +1,9 @@
 import json
 
-# import requests
-
-
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello from github actions with aws sam",
-            # "location": ip.text.replace("\n", "")
         }),
     }
